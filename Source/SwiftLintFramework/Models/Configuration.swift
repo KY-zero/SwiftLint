2134f99f524d439a7e03a638972e666b76ed57a1
JP Simard <jp@jpsim.com>
2017-07-13 17:27:06 -0700

    move "lintable files" functionality of Configuration to its own file

--- Source/SwiftLintFramework/Models/Configuration.swift
+++ Source/SwiftLintFramework/Models/Configuration.swift
@@ -179,28 +179,6 @@ public struct Configuration: Equatable {
             (lhs.rootPath == lhs.rootPath) &&
             (lhs.rules == rhs.rules)
     }
-
-    // MARK: Lintable Files
-
-    public func lintableFiles(inPath path: String) -> [File] {
-        return lintablePaths(inPath: path).flatMap(File.init(path:))
-    }
-
-    internal func lintablePaths(inPath path: String,
-                                fileManager: LintableFileManager = FileManager.default) -> [String] {
-        // If path is a Swift file, skip filtering with excluded/included paths
-        if path.bridge().isSwiftFile() && path.isFile {
-            return [path]
-        }
-        let pathsForPath = included.isEmpty ? fileManager.filesToLint(inPath: path, rootDirectory: nil) : []
-        let excludedPaths = excluded.flatMap {
-            fileManager.filesToLint(inPath: $0, rootDirectory: rootPath)
-        }
-        let includedPaths = included.flatMap {
-            fileManager.filesToLint(inPath: $0, rootDirectory: rootPath)
-        }
-        return (pathsForPath + includedPaths).filter({ !excludedPaths.contains($0) })
-    }
 }
 
 // MARK: Identifier Validation
